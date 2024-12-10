148e8b59266a85b8c49ebe701afb1c9071c77e1e
ireshcodex <ireshgopanpalli126@gmail.com>
2024-12-10 12:43:48 +0530

    Lens Info added

--- src/Component/IreshG/Homepage.jsx
+++ src/Component/IreshG/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function IreshBranch() {
+function Homepage() {
     return (
         <>
             <div>I'm Iresh at same branch from</div>
@@ -9,4 +9,4 @@ function IreshBranch() {
     )
 }
 
-export default IreshBranch
+export default Homepage;
